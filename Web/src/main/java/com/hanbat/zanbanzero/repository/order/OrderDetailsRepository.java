607308905d90313e74e83f80e67d69c1786569a8
HyeongMokJeong <jhm1062@naver.com>
2023-03-02 02:04:56 +0900

    update query

--- Web/src/main/java/com/hanbat/zanbanzero/repository/order/OrderDetailsRepository.java
+++ Web/src/main/java/com/hanbat/zanbanzero/repository/order/OrderDetailsRepository.java
@@ -2,14 +2,15 @@ package com.hanbat.zanbanzero.repository.order;
 
 import com.hanbat.zanbanzero.entity.order.Order;
 import com.hanbat.zanbanzero.entity.order.OrderDetails;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Order> {
 
-    @Query("select o from OrderDetails o join fetch o.orders where o.id = :id")
-    //@EntityGraph(attributePaths = {})
+    //이전 쿼리 @Query("select o from OrderDetails o join fetch o.orders where o.id = :id")
+    @Query(value = "select * " +
+            "from (select * from order_details where order_details.orders_id = :id) o " +
+            "join orders on o.orders_id = orders.id", nativeQuery = true)
     OrderDetails getOrderDetails(@Param("id") Long id);
 }
